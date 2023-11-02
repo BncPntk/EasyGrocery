e0f03105beb63719c904d1e95fd20655f2cd250c
Bence Pentek <pntkbence@gmail.com>
2023-11-02 15:16:46 +0100

    fix: emoji on 1st word

--- easy_grocery/src/components/Item.js
+++ easy_grocery/src/components/Item.js
@@ -2,6 +2,7 @@ export default function Item({ item, items, setItems }) {
   const emoji = require('emoji-dictionary');
 
   let formattedName = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+  const extractedName = formattedName.split(' ');
 
   function handleBought() {
     setItems((prevItems) => {
@@ -13,12 +14,11 @@ export default function Item({ item, items, setItems }) {
       });
     });
   }
-
   return (
     <div className={`item ${item.checked ? 'bought_item' : ''}`} onClick={handleBought}>
-      <h3>{emoji.getUnicode(formattedName.toLowerCase()) || formattedName.charAt(0)}</h3>
+      <h3>{emoji.getUnicode(extractedName[0].toLowerCase()) || formattedName.charAt(0)}</h3>
       <p className={formattedName.length > 10 ? 'item_text_small' : 'item_text_normal'}>
-        {formattedName.length > 34 ? formattedName.slice(0, 31) + '...' : formattedName}
+        {formattedName.length > 34 ? `${formattedName.slice(0, 31)}...` : formattedName}
       </p>
     </div>
   );
